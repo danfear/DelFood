b2331b4586e29c96c4bd4003f0ef4274970e315c
afdiaz97 <afdm970101@gmail.com>
2022-09-01 19:24:47 -0500

    inicio springboot

---
+++ delfood/src/main/java/com/delfood/delfood/controllers/controler.java
@@ -0,0 +1,19 @@
+package com.delfood.delfood.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class controler {
+    @RequestMapping("")
+	public String Inicio(Model modelo) {
+		return "pagina";
+	}  
+    @RequestMapping("/domicilio")
+	public String Domicilio(Model modelo) {
+		return "pagina2";
+	}  
+    @RequestMapping("/confirmacion")
+	public String Confirmacion(Model modelo) {
+		return "pagina3";
+	}  
+}
---
+++ delfood/src/test/java/com/delfood/delfood/DelfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delfood.delfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
