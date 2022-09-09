18251cedfcf8d7244786fc55fc6900e92b2d3830
afdiaz97 <afdm970101@gmail.com>
2022-09-08 20:37:23 -0500

    Entidad platos

---
+++ delfood/src/main/java/com/delfood/delfood/Models/Platos.java
@@ -0,0 +1,56 @@
+package com.delfood.delfood.Models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Platos")
+public class Platos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @Column
+    private String name;
+    @Column
+    private Double precio;
+    @Column
+    private String descripcion;
+
+    public Long getId(){
+        return Id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Double getPrecio(){
+        return precio;
+    }
+
+    public String getDescription(){
+        return descripcion;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void SetPrecio(double precio){
+        this.precio=precio;
+    }
+
+    public void setdescription(String description){
+        this.descripcion=description;
+    }
+
+    public plato(String name,double precio, String description){
+        this.name=name;
+        this.precio=precio;
+        this.descripcion=description;
+    }
+}
