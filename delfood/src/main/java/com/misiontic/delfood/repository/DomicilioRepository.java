8e82619dfc4777cfdcd58c79b8773d4784fef34a
danfear <105298880+danfear@users.noreply.github.com>
2022-10-03 19:51:40 -0500

    Delete delfood directory

--- delfood/src/main/java/com/misiontic/delfood/repository/DomicilioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.misiontic.delfood.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.misiontic.delfood.models.Domicilio;
-
-public class DomicilioRepository extends JpaRepository<Domicilio, Long>{
-    
-}
--- delfood/src/main/java/com/misiontic/delfood/repository/PlatoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.misiontic.delfood.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.misiontic.delfood.models.Plato;
-
-public class PlatoRepository extends JpaRepository<Plato, Long>{
-    
-}
