83725d933bc4e6b1e3259186ba7d168f4a5ffb28
danfear <105298880+danfear@users.noreply.github.com>
2022-10-03 19:48:25 -0500

    Update PlatoService.java

--- delfood/src/main/java/com/misiontic/delfood/service/PlatoService.java
+++ delfood/src/main/java/com/misiontic/delfood/service/PlatoService.java
@@ -1,23 +1,22 @@
-package com.misiontic.delfood.service;
-
-import com.misiontic.delfood.repository.PlatoRepository;
+package com.delfood.delfood.service;
+import com.delfood.delfood.repository.PlatoRepository;
 import org.springframework.stereotype.Service;
-import com.misiontic.delfood.models.Plato;
+import com.delfood.delfood.Models.Plato;
 import java.util.List;
 
 @Service
 public class PlatoService {
-    private final PlatoRepository platorepository;
+  private final PlatoRepository platorepository;
   
-    public PlatoService(PlatoRepository platorepository){
-      this.platorepository=platorepository;
-    }
+  public PlatoService(PlatoRepository platorepository){
+    this.platorepository=platorepository;
+  }
 
-    public List<Plato> buscartodos(){
-      return platorepository.findAll();
-    } 
-    public Plato getplatobyId(Long Id){
-      Plato plato=platorepository.findById(Id).orElseThrow(() -> new RuntimeException("No existe el plato"));
-      return plato;
-    }
+  public List<Plato> buscartodos(){
+    return platorepository.findAll();
+  } 
+  public Plato getplatobyId(Long Id){
+    Plato plato=platorepository.findById(Id).orElseThrow(() -> new RuntimeException("No existe el plato"));
+    return plato;
+  }
 }
