b1afeff05d0ae8591e452dd7ef6697b1d54b0411
danfear <dfariasc64@gmail.com>
2022-10-05 17:42:05 -0500

    despliegue en proceso

---
+++ src/main/resources/schema_db.sql
@@ -0,0 +1,59 @@
+INSERT INTO `platos`(
+    `descripcion`,
+    `image`,
+    `name`,
+    `precio`,
+    `tiempo`
+)
+VALUES(
+    'Ensalada de lechuga, huevo cocido, aguacate, tomate y camarones',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy-dYsw3gvOhLHd4phwuxtfA4FltAyicc4tg&usqp=CAU',
+    'Ensalada gourmet',
+    16000,
+    15
+)
+
+INSERT INTO `platos`(
+    `descripcion`,
+    `image`,
+    `name`,
+    `precio`,
+    `tiempo`
+)
+VALUES(
+    'Pasta en salsa carbonara, queso parmesano, aceite de oliva y tocino',
+    'https://canalcocina.es/medias/publicuploads/2013/08/28/115707/nu6sasvfsmeq90a9e33i.jpg',
+    'Pasta carbonara',
+    19000,
+    20
+)
+
+INSERT INTO `platos`(
+    `descripcion`,
+    `image`,
+    `name`,
+    `precio`,
+    `tiempo`
+)
+VALUES(
+    'Sandwich de jamón, queso cheddar, tomate, lechuga y cebolla',
+    'https://images.aws.nestle.recipes/original/7f51739bcb332523281418c1349c605b_sandwichito-1920x1005.jpg',
+    'Sandwich de queso',
+    12000,
+    10
+)
+
+INSERT INTO `platos`(
+    `descripcion`,
+    `image`,
+    `name`,
+    `precio`,
+    `tiempo`
+)
+VALUES(
+    'Lasagna de carne, pasta en láminas, queso parmesano y salsa',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj_ScFLLL7maTFWzv6QRmygfbO1NpdGQMOUO76-J31Pb--skMRe-n3RQJ8NN0eClh3UPo&usqp=CAU',
+    'Lasagna bolognesa',
+    17000,
+    25
+)
